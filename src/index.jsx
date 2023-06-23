f41a5d0af61f2aa8450dd2512f8dd8606c2216c7
Camille <camille.mathien@gmail.com>
2023-06-23 17:01:34 +0200

    prepare link to freelance profile

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
+import Profile from './pages/Profile'
 import Header from './components/Header'
 import Error from './components/Error'
 import { ThemeProvider, SurveyProvider } from './utils/context/index'
@@ -26,6 +27,7 @@ root.render(
             <Route path="/survey/:questionNumber" element={<Survey />} />
             <Route path="/results" element={<Results />} />
             <Route path="/freelances" element={<Freelances />} />
+            <Route path="/profile/:id" element={<Profile />} />
             <Route path="*" element={<Error />} />
           </Routes>
           <Footer />
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import colors from '../../utils/style/color'
 import { Loader } from '../../utils/style/Atoms'
 import { useFetch, useTheme } from '../../utils/hooks'
+import { Link } from 'react-router-dom'
 
 const CardsContainer = styled.div`
   display: grid;
@@ -59,12 +60,14 @@ function Freelances() {
       ) : (
         <CardsContainer>
           {freelancersList?.map((profile, index) => (
+            <Link key={`freelance-${profile.id}`} to={`/profile/${profile.id}`}>
             <Card
               key={`${profile.name}-${index}`}
               label={profile.job}
               title={profile.name}
               picture={profile.picture}
             />
+            </Link>
           ))}
         </CardsContainer>
       )}
